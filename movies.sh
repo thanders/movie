ad78f868bf021a9cd586e7a2b7a9fb114b375c46
thanders <thanders@alumni.purdue.edu>
2018-01-16 21:21:47 +0100

    new updates

--- movies.sh
+++ movies.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Checks for connection to google.com from local port 443
 if nc -zw1 google.com 443; then
 	echo "Internet connection works over HTTPS (port 443)"
 	./process_movies.sh
+# Checks for connection to google.com from local port 80
 elif nc -zw1 google.com 80; then
 	echo "Internet connection works over HTTP (port 80)"
 	./process_movies.sh
@@ -10,12 +12,16 @@ else
 	echo "Internet connection isn't working over HTTP or HTTPS (ports 80 or 443)"
 fi
 
-if [[ -e movies.txt ]]; then
-	wc=`wc -l < movies.txt`
-	echo "$wc movies are playing in your area."
+# If movies.txt exists:
+if [[ -e scrape/movies.txt ]]; then
+	#Line count:
+	wc=`wc -l < scrape/movies.txt`
+	echo $wc " movies are playing in your area."
+	#New line
 	echo -e '\n'
-	column -ts $'\t' movies.txt
+	cat scrape/movies.txt | sort -b -r -t$'#' -k 2.1,2.4 -k 2.6,2.7 -k 2.9,2.10 | column -ts $'#'
 fi
 
 # Remove the movies.txt file
-rm movies.txt
+rm -rf scrape
+
--- process_movies.sh
+++ process_movies.sh
@@ -3,16 +3,29 @@ mkdir scrape
 # Download updated file
 wget -qO scrape/data.html www.imdb.com/showtimes/location?ref_=sh_lc
 sed -i 's/hidden inline-sort-params/dingle/g' scrape/data.html
-hxnormalize -ex scrape/data.html | hxselect -s '\n' 'div.dingle' >> scrape/output.html
+hxnormalize -ex scrape/data.html | hxselect -s '\n' 'div.dingle' | tr -s [:space:] ' ' | sed 's/<\/div>/\'$'\n/g' >> scrape/output.txt
 # Define the internal field separator as a new line
 IFS=$'\n'
 # Parse each group by searching for each variable and cutting the data
-for line in $(cat scrape/output.html); do
-	title=`echo -n $line | grep 'alpha' | cut -d '"' -f 8,8`
-	release_date=`echo -n $line | grep 'release_date' | cut -d '"' -f 16,16`
-	echo -e $title '\t' $release_date >> scrape/parsed.txt
-done
-# Remove spaces and create movies.txt
-egrep -v "^[[:space:]]*$|^#" scrape/parsed.txt >> movies.txt
+# For each div dingle
 
-rm -rf scrape
+title=()
+release_date=()
+
+for line in $(cat scrape/output.txt); do
+	title+=("`echo $line | grep 'alpha' | cut -d '"' -f 8,8`")
+	release_date+=("`echo $line | grep -C 1 'release_date' | cut -d '"' -f 16,16`")
+done
+
+n=${#title}
+
+for ((i=0;i<$n;i++)); do
+	echo "${title[$i]}"#"${release_date[$i]}" >> scrape/parsed.txt;
+	
+done
+
+# Removes lines with 1 character (,) , adds / to dates, replaes // with nothing
+awk 'length>1' scrape/parsed.txt | awk -F '#' '{ print $1 "#" substr($2,1,4)"/"substr($2,5,2)"/"substr($2,7,2) }' | sed 's|//||g' >> scrape/movies.txt
+#tr -d "v" < scrape/parsed.txt
+
+#rm -rf scrape

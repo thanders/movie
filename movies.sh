046118d3d7c51e2a7152c4000bafcd37b50a91eb
thanders <thanders@alumni.purdue.edu>
2018-05-02 19:35:43 +0100

    Added a comment to explain sort

--- movies.sh
+++ movies.sh
@@ -20,6 +20,7 @@ today=$(date)
 echo "Movie data as of: $today"
 echo "Movies playing in your area: $number_mov"
 echo -e '\n'
+# The sort below sorts by column k and then the number of digits until next k. The date string has 12 characters including an initial blank
 cat processed_files/parsed_output.csv | awk '{gsub("Release:", ""); gsub("Runtime:", ""); gsub(" minutes", "");print}' | awk 'NR<2{print $0;next}{print $0 | "sort -b -t '\t' -k 1.9,1.12nr  -k 1.5,1.7Mr -k 1.1,1.3nr"}' | column -ts $'\t'
 
 # Remove the movies.txt file
